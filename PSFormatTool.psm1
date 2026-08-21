e5974191dfceb0414a716b438b103d88f1d0faa4
Ara <aragornaed@gmail.com>
2026-08-21 14:30:49 +0100

    Refactored

--- PSFormatTool.psm1
+++ PSFormatTool.psm1
@@ -1,52 +1,3 @@
-function Initialize-Format {
-<#
-.SYNOPSIS
-Formats a selected volume.
-
-.PARAMETER DriveLetter
-Specifies the drive letter of the volume to format. Provide one letter without
-a trailing colon. The operating system volume cannot be formatted.
-
-.PARAMETER FileSystem
-Specifies the file system to apply. The supported values are exFAT and NTFS.
-
-.PARAMETER NewName
-Specifies the new label for the formatted volume. For exFAT volumes, the label
-cannot contain more than 11 characters.
-#>
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidatePattern('^[A-Za-z]$')]
-        [String]$DriveLetter,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateSet("exFAT", "NTFS")]
-        [String]$FileSystem,
-
-        [String]$NewName
-    )
-    $DiskInfo = Get-Volume -DriveLetter $DriveLetter -ErrorAction SilentlyContinue
-    if (-not $DiskInfo) {
-        throw "The specified volume could not be found."
-    }
-    if ($DriveLetter -eq $env:SystemDrive.TrimEnd(':')) {
-        throw "The selected volume contains the operating system and cannot be modified."
-    }
-    if ($FileSystem -eq "exFAT") {
-        if ($NewName.length -gt 11) {
-            throw "You cannot enter more than 11 characters for the label in the exFat format."
-        }
-    }
-    if ($PSCmdlet.ShouldProcess("Volume $($DiskInfo.FileSystemLabel) - $($DiskInfo.DriveLetter) - $($DiskInfo.DriveType)", "All data will be deleted for a new format.")) {
-        try {
-            Format-Volume -DriveLetter $DriveLetter -FileSystem $FileSystem -NewFileSystemLabel $NewName -ErrorAction Stop | Out-Null
-            Write-Verbose "Format completed successfully."
-        }   catch {
-            $PSCmdlet.ThrowTerminatingError($_)
-        }
-    }
-}
 function New-Format {
 <#
 .SYNOPSIS
@@ -95,6 +46,55 @@ administrator privileges. It supports -WhatIf and -Confirm.
             @PSBoundParameters
     }   catch {
         $PSCmdlet.ThrowTerminatingError($_)
+    }
+}
+function Initialize-Format {
+<#
+.SYNOPSIS
+Formats a selected volume.
+
+.PARAMETER DriveLetter
+Specifies the drive letter of the volume to format. Provide one letter without
+a trailing colon. The operating system volume cannot be formatted.
+
+.PARAMETER FileSystem
+Specifies the file system to apply. The supported values are exFAT and NTFS.
+
+.PARAMETER NewName
+Specifies the new label for the formatted volume. For exFAT volumes, the label
+cannot contain more than 11 characters.
+#>
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidatePattern('^[A-Za-z]$')]
+        [String]$DriveLetter,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("exFAT", "NTFS")]
+        [String]$FileSystem,
+
+        [String]$NewName
+    )
+    $DiskInfo = Get-Volume -DriveLetter $DriveLetter -ErrorAction SilentlyContinue
+    if (-not $DiskInfo) {
+        throw "The specified volume could not be found."
+    }
+    if ($DriveLetter -eq $env:SystemDrive.TrimEnd(':')) {
+        throw "The selected volume contains the operating system and cannot be modified."
+    }
+    if ($FileSystem -eq "exFAT") {
+        if ($NewName.length -gt 11) {
+            throw "You cannot enter more than 11 characters for the label in the exFat format."
+        }
+    }
+    if ($PSCmdlet.ShouldProcess("Volume $($DiskInfo.FileSystemLabel) - $($DiskInfo.DriveLetter) - $($DiskInfo.DriveType)", "All data will be deleted for a new format.")) {
+        try {
+            Format-Volume -DriveLetter $DriveLetter -FileSystem $FileSystem -NewFileSystemLabel $NewName -ErrorAction Stop | Out-Null
+            Write-Verbose "Format completed successfully."
+        }   catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
     }
 }
 Export-ModuleMember -Function New-Format
